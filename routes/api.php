589bb982c2f4e40e9fabe06828472b6c97961b70
toka99 <toka_attiah@gmail.com>
2021-05-28 00:27:24 +0200

    starting restaurant/rating model -a mariam rewan toka

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/restaurants','App\Http\Controllers\RestaurantController');
+Route::group(['prefix'=>'restaurants'],function(){
+    Route::apiResource('/{restaurant}/ratings','App\Http\Controllers\RatingController');
+});
+
