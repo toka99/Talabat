ff0c6e595c6f42c6bcb1421a04222aa22c594345
MaryemElSaeid <maryemelsaeid@gmail.com>
2021-06-09 17:39:23 +0200

    Fixing bugs and Creating admin seeder toka-rewan-maryem

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
+
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,22 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+       DB::table('users')->insert([
+
+        'first_name' => 'admin',
+        'last_name' => 'admin',
+        'email' => 'admin@admin.com',
+        'password' => '$2a$04$yFHQomp6QfcZpQDwF5Gj5egp8yKTa3rG3Qs54gyTGdBzqlKdwbh8G',
+        'gender' => 'Female' ,
+        'date_of_birth' => '1960-12-17',
+        'mobile_number' => '201111111111',
+        'created_at' => '2021-06-09 17:57:23',
+        'updated_at' => '2021-06-09 17:57:23'
+]);
+ 
+
     }
 }
+
+
+
