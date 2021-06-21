ef806e589037da2e849d09a09f4cb64f6fdf3826
toka99 <toka_attiah@gmail.com>
2021-06-22 01:48:10 +0200

    some modification rewan-mariam-toka

--- app/Http/Resources/Restaurant/RestaurantResource.php
+++ app/Http/Resources/Restaurant/RestaurantResource.php
@@ -16,6 +16,7 @@ class RestaurantResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'logo' => $this->logo,
